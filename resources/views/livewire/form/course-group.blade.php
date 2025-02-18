4f34d993f65863a14676365448c0a1de60445edc
masif088 <mokhamadasif@gmail.com>
2025-02-19 00:02:26 +0700

    add next url on admin

--- resources/views/livewire/form/course-group.blade.php
+++ resources/views/livewire/form/course-group.blade.php
@@ -2,6 +2,6 @@
     <x-input title="Title" model="title" required="true"/>
     <x-input title="Sub Title" model="subTitle" required="true"/>
     <x-select title="Type" model="type" :options="$optionCourseType" required="true"/>
-    <x-select2 title="Course list" model="type" :options="$optionCourseType" required="true"/>
+    <x-select2 title="Course list" model="courseLists" :options="$optionCourseTitle" required="true"/>
     <button type="submit" class="btn" wire:loading.attr="disabled">Submit</button>
 </form>
