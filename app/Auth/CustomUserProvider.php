1302aaea548f20a05ba4178eba911b84f01ce300
masif088 <mokhamadasif@gmail.com>
2025-05-13 20:53:42 +0700

    + time + search on activity

---
+++ app/Auth/CustomUserProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Auth;
+
+use Illuminate\Auth\EloquentUserProvider;
+use Illuminate\Contracts\Auth\Authenticatable;
+
+class CustomUserProvider extends EloquentUserProvider
+{
+    public function validateCredentials(Authenticatable $user, array $credentials)
+    {
+        $plain = $credentials['password'];
+        $hashed = $user->getAuthPassword();
+
+        if (str_starts_with($hashed, '$wp$')) {
+            $hashed = str_replace('$wp$', '$', $hashed);
+            return password_verify($plain, $hashed);
+        }
+
+        if (str_starts_with($hashed, '$P$')) {
+            // Butuh library PHPass kalau kamu ingin dukung hash lama
+            return $this->checkPhpPass($plain, $hashed);
+        }
+
+        return parent::validateCredentials($user, $credentials);
+    }
+
+    protected function checkPhpPass($password, $hash)
+    {
+        // kamu bisa pakai library ini via composer:
+        // composer require hautelook/phpass
+
+        $hasher = new \Hautelook\Phpass\PasswordHash(8, true);
+        return $hasher->CheckPassword($password, $hash);
+    }
+}
