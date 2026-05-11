5a7e9a03a753a07f2ab42bf5e08c9e2f14da808f
masif088 <mokhamadasif@gmail.com>
2026-05-12 00:51:08 +0700

    course group just show for if chart 1

--- resources/views/admin/company-portal/users.blade.php
+++ resources/views/admin/company-portal/users.blade.php
@@ -6,8 +6,8 @@
                     <h1 class="text-2xl font-bold text-gray-900 dark:text-light">Company users</h1>
                     <p class="text-sm text-gray-600 dark:text-gray-400">Search and manage users linked to your company only.</p>
                 </div>
-                <a href="{{ route('company.portal.users.create') }}" class="btn btn-primary shrink-0">Tambah user</a>
             </div>
+            <a href="{{ route('company.portal.users.create') }}" class="btn btn-primary shrink-0">Tambah user</a>
             <livewire:table.master wire:key="company-portal-users-{{ $companyId }}" name="User" :param1="$companyId"/>
         </div>
     </div>
