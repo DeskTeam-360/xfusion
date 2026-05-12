2fcf2c1d62a6b23164c62911b10ce96119ddc010
masif088 <mokhamadasif@gmail.com>
2026-05-13 02:48:02 +0700

    scoring

--- resources/views/admin/course-scoring-group/edit.blade.php
+++ resources/views/admin/course-scoring-group/edit.blade.php
@@ -1,7 +1,7 @@
 <x-admin-layout>
     <div class="container full-container py-5">
         <div class="mb-6 px-5 flex flex-wrap gap-3">
-            <a href="{{ route('course-scoring-group.index') }}" class="btn-sm">Back to list</a>
+            <a href="{{ route('course-scoring-group.index') }}" class="btn-sm btn">Back to list</a>
         </div>
         <div class="px-5">
             <h1 class="mb-6 text-2xl font-semibold text-dark dark:text-white">Edit course scoring group</h1>
