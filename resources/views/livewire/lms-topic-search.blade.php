51d63abad212eb79279e2d4de589c0563f118260
masif088 <mokhamadasif@gmail.com>
2026-06-18 05:47:51 +0700

    make UI better

--- resources/views/livewire/lms-topic-search.blade.php
+++ resources/views/livewire/lms-topic-search.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full">
-    <form wire:submit.prevent="search" class="flex w-full max-w-3xl flex-col gap-4 sm:flex-row sm:items-end mt-10">
+    <form wire:submit.prevent="search" class="flex w-full max-w-3xl flex-col gap-4 sm:flex-row sm:items-end m-10">
         
         <div class="flex-1">
             <input
